e854a5d399eb9444399a90509ae2c7988b9053a1
Jérémy Levilain <jeremy@jeremylvln.fr>
2021-12-28 20:38:11 +0100

    init

---
+++ mods/slimmythings/src/main/java/fr/iamblueslime/slimmythings/SlimmyThings.java
@@ -0,0 +1,11 @@
+package fr.iamblueslime.slimmythings;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(SlimmyThings.MODID)
+public class SlimmyThings {
+    public static final String MODID = "slimmythings";
+    private static final Logger LOGGER = LogManager.getLogger();
+}
