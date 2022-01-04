caaf2abfa5b9a8bfed82cd3a7a8335695b7fafab
Jérémy Levilain <jeremy@jeremylvln.fr>
2022-01-04 20:15:23 +0100

    refactor(keepmykeys): move client setup to mod class

--- mods/keepmykeys/src/main/java/fr/iamblueslime/keepmykeys/client/ClientEventListener.java
+++ mods/keepmykeys/src/main/java/fr/iamblueslime/keepmykeys/client/ClientEventListener.java
@@ -9,17 +9,11 @@ import net.minecraft.client.gui.screens.controls.ControlsScreen;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ScreenEvent;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = KeepMyKeys.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientEventListener {
-    public static void init(final FMLClientSetupEvent ignoredEvent) {
-        MinecraftForge.EVENT_BUS.register(new ClientEventListener());
-    }
-
     @SubscribeEvent
     public void onPostInitScreen(final ScreenEvent.InitScreenEvent.Post event) {
         Screen screen = event.getScreen();
